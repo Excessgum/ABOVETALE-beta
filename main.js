d4a3131bd19d959e028c8a80f2837d8a6610dec5
あまるがむ <154035006+Excessgum@users.noreply.github.com>
2026-02-07 11:18:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -195,25 +195,29 @@ function drawCircle(x, y, r, color) {
 }
 
 function repaint() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, 900, 600);
+ ctx.fillStyle = "black";
+ctx.fillRect(0, 0, 900, 600);
 
-  ctx.save();
-  ctx.beginPath();
-  ctx.rect(300, 300, 300, 200);
-  ctx.clip();
+ctx.save();   // ★いちばん最初に保存！
 
-  ctx.fillStyle = "brown";
-  ctx.translate(6 * 50, 6 * 50);
-  ctx.translate(-player.getScrollX(), -player.getScrollY());
+ctx.translate(6 * 50, 6 * 50);
+ctx.translate(-player.getScrollX(), -player.getScrollY());
 
-  for (let x = 0; x < W; x++) {
-    for (let y = 0; y < H; y++) {
-      if (maze[y][x] == 1) {
-        ctx.fillRect(x * 50, y * 50, 50, 50);
-      }
+ctx.beginPath();
+ctx.rect(300, 300, 300, 200);
+ctx.clip();
+
+ctx.fillStyle = "brown";
+for (let x = 0; x < W; x++) {
+  for (let y = 0; y < H; y++) {
+    if (maze[y][x] == 1) {
+      ctx.fillRect(x * 50, y * 50, 50, 50);
     }
   }
+}
+
+ctx.restore();  // ★これ超重要！！
+
 
   aliens.forEach(a => a.paint(ctx, 50, 50));
   ctx.restore();
